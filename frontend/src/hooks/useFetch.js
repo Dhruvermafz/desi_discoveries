e6ec05be35e57bc3a8e936f1e8abc78c20b4d48e
dhruvermafz <vermadhruv09112002@gmail.com>
2024-07-02 15:10:57 +0530

    header footer booking added

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/config";
+
+const useFetch = (endpoint, queryParams) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await axios.get(`${BASE_URL}/${endpoint}`, {
+          params: queryParams,
+        });
+        setData(response.data.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [endpoint, queryParams]);
+  return {
+    data,
+    error,
+    loading,
+  };
+};
+
+export default useFetch;
